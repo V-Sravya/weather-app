62396a23c717be661dd4e0534d885fdb4eceda93
Sravya Sri Virigineni <sravyasrivirigineni2224@gmail.com>
2025-02-26 18:45:32 -0600

    Update fetch url in script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const response = await fetch(`http://localhost:3000/weather?city=${cityInput}`);
+      const response = await fetch(`https://my-weather-app-xumm.onrender.com/weather?city=${cityInput}`);
       const data = await response.json();
 
       if (data && data.city) 
